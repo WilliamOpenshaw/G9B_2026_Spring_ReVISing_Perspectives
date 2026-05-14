0a5a124bcc6dfe15e32a7963205cdecb0d94e61b
joshuama0109 <joshua.ma@vis.tp.edu.tw>
2026-05-14 15:28:56 +0800

    wt 0.2.unity

--- Assets/Scripts/wt_movement_blue.cs
+++ Assets/Scripts/wt_movement_blue.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class wt_movement_blue : MonoBehaviour
 {
+
+    public float speed = 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,21 +17,22 @@ public class wt_movement_blue : MonoBehaviour
         // move character ui image up with rect transform anchor in canvas when w key is pressed
         if (Input.GetKey(KeyCode.W))
         {
-            GetComponent<RectTransform>().anchoredPosition += new Vector2(0, 0.2f);
+            GetComponent<RectTransform>().anchoredPosition += new Vector2(0, speed);
         }
         // move character ui imnage down with rect transform anchor in canvas when s key is pressed
         if (Input.GetKey(KeyCode.S))        {
-            GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -0.2f);
+            GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -speed);
         }
          // move character ui image left with rect transform anchor in canvas when a key is pressed
         if (Input.GetKey(KeyCode.A))        {
-            GetComponent<RectTransform>().anchoredPosition += new Vector2(-0.2f, 0);
+            GetComponent<RectTransform>().anchoredPosition += new Vector2(-speed, 0);
         }
          // move character ui image right with rect transform anchor in canvas when d key is pressed
         if (Input.GetKey(KeyCode.D))        {
-            GetComponent<RectTransform>().anchoredPosition += new Vector2(0.2f, 0);
+            GetComponent<RectTransform>().anchoredPosition += new Vector2(speed, 0);
         }
 
     
     }
 }
+// 
