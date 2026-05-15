c85ffba0bfd2a3f89c2c97ee13913a96533f824e
rubie-stack <rubie.huang@vis.tp.edu.tw>
2026-05-15 14:36:01 +0800

    did some things

--- Assets/Scenes/Final Rubric/white strawberry/scripts/moneymanager.cs
+++ Assets/Scenes/Final Rubric/white strawberry/scripts/moneymanager.cs
@@ -6,10 +6,19 @@ using TMPro;
 public class moneymanager : MonoBehaviour
 {
     public TMP_Text moneyText;
-    public int currentMoney = 0;
+    public int startMoney = -1000;
+    public int currentMoney;
+
+    void Awake()
+    {
+        currentMoney = startMoney;
+    }
 
     void Start()
     {
+        if (moneyText == null)
+            Debug.LogWarning("moneymanager: moneyText is not assigned in the Inspector.");
+
         UpdateMoneyText();
     }
 
@@ -17,19 +26,19 @@ public class moneymanager : MonoBehaviour
     {
         currentMoney += amount;
         UpdateMoneyText();
+        Debug.Log($"AddMoney called: {amount}. Current money = {currentMoney}");
     }
 
     public void SubtractMoney(int amount = 1)
     {
         currentMoney -= amount;
-        if (currentMoney < 0)
-            currentMoney = 0;
         UpdateMoneyText();
+        Debug.Log($"SubtractMoney called: {amount}. Current money = {currentMoney}");
     }
 
     void UpdateMoneyText()
     {
         if (moneyText != null)
-            moneyText.text = "Money: " + currentMoney.ToString();
+            moneyText.text = "$" + currentMoney.ToString();
     }
 }
