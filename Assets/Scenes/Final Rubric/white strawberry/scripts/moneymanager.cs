46bcbdd31ffdf1fe36546673d3aec6ce2ab615a3
rubie-stack <rubie.huang@vis.tp.edu.tw>
2026-05-19 08:59:48 +0800

    Update moneymanager.cs

--- Assets/Scenes/Final Rubric/white strawberry/scripts/moneymanager.cs
+++ Assets/Scenes/Final Rubric/white strawberry/scripts/moneymanager.cs
@@ -9,17 +9,57 @@ public class moneymanager : MonoBehaviour
     public int startMoney = -1000;
     public int currentMoney;
 
+    int lastMoney;
+
     void Awake()
     {
         currentMoney = startMoney;
+        lastMoney = currentMoney;
     }
+
+    bool hasWarnedNoText;
 
     void Start()
     {
         if (moneyText == null)
-            Debug.LogWarning("moneymanager: moneyText is not assigned in the Inspector.");
+            moneyText = GetComponent<TMP_Text>();
+
+        if (moneyText == null)
+            moneyText = GetComponentInChildren<TMP_Text>();
+
+        if (moneyText == null)
+        {
+            var allTexts = Object.FindObjectsByType<TMP_Text>(FindObjectsSortMode.None);
+            foreach (var text in allTexts)
+            {
+                if (text.gameObject.name.ToLower().Contains("money") || text.gameObject.name.ToLower().Contains("cash"))
+                {
+                    moneyText = text;
+                    break;
+                }
+            }
+
+            if (moneyText == null && allTexts.Length > 0)
+                moneyText = allTexts[0];
+        }
+
+        if (moneyText == null)
+        {
+            Debug.LogWarning("moneymanager: moneyText is not assigned in the Inspector and no TMP_Text was found.");
+            hasWarnedNoText = true;
+        }
+        else
+        {
+            Debug.Log("moneymanager: moneyText auto-assigned to " + moneyText.gameObject.name);
+        }
 
         UpdateMoneyText();
+    }
+
+    void Update()
+    {
+        if (currentMoney != lastMoney)
+            UpdateMoneyText();
     }
 
     public void AddMoney(int amount = 1)
@@ -39,6 +79,14 @@ public class moneymanager : MonoBehaviour
     void UpdateMoneyText()
     {
         if (moneyText != null)
+        {
             moneyText.text = "$" + currentMoney.ToString();
+            lastMoney = currentMoney;
+        }
+        else if (!hasWarnedNoText)
+        {
+            Debug.LogWarning("moneymanager: Cannot update money display because moneyText is missing.");
+            hasWarnedNoText = true;
+        }
     }
 }
