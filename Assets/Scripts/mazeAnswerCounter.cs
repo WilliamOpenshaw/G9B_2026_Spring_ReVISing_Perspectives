ed1d911b83fe6b99872f112b88392ed5290c4c35
Ann Hsiao <ann.hsiao@vis.tp.edu.tw>
2026-06-12 23:26:07 +0800

    maze 0.0.6
    the version that we fix after the teacher's suggestion

--- Assets/Scripts/mazeAnswerCounter.cs
+++ Assets/Scripts/mazeAnswerCounter.cs
@@ -1,100 +1,127 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class mazeAnswerCounter : MonoBehaviour
 {
-    // answer list
+    // 正確答案
     public string[] correctAnswer = new string[14];
-    // user choices list empty for now, will be filled with user choices in the future
+
+    // 玩家答案
     public string[] userChoices = new string[14];
 
-    public GameObject nextWinScreen; // Reference to the next win screen GameObject
-    public GameObject failScreen; // Reference to the fail screen GameObject
+    public GameObject nextWinScreen;
+    public GameObject failScreen;
+    public GameObject currentScreen;
 
-    public GameObject currentScreen; // Reference to the current screen GameObject
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // 14個勾勾位置
+    public Image[] answerMarks;
+
+    // 勾勾圖片
+    public Sprite checkSprite;
+
+    // 叉叉圖片
+    public Sprite crossSprite;
+
+    public int index = 0;
+
     void Start()
     {
-        correctAnswer = new string[] { "buying", 
-                                       "cooking", 
-                                "medicine", 
-                                "cleaning", 
-                                "washing", 
-                                "cooking", 
-                                "medicine", 
-                                "dishes", 
-                                "folding", 
-                                "garbage", 
-                                "cooking", 
-                                "medicine", 
-                                "shower", 
-                                "book" };
-        // user choices will be filled with user choices in the future, for now it is empty
-        userChoices = new string[] { "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+        SetupAnswers();
+        HideAllMarks();
     }
 
     void OnEnable()
     {
-        correctAnswer = new string[] { "buying", 
-                                       "cooking", 
-                                "medicine", 
-                                "cleaning", 
-                                "washing", 
-                                "cooking", 
-                                "medicine", 
-                                "dishes", 
-                                "folding", 
-                                "garbage", 
-                                "cooking", 
-                                "medicine", 
-                                "shower", 
-                                "book" };
-        // user choices will be filled with user choices in the future, for now it is empty
-        userChoices = new string[] { "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+        SetupAnswers();
+        HideAllMarks();
     }
 
+    void SetupAnswers()
+    {
+        correctAnswer = new string[]
+        {
+            "buying",
+            "cooking",
+            "medicine",
+            "cleaning",
+            "washing",
+            "cooking",
+            "medicine",
+            "dishes",
+            "folding",
+            "garbage",
+            "cooking",
+            "medicine",
+            "shower",
+            "book"
+        };
 
-    // function to take string and add it to userchoices list
-    // find next empty slot in userchoices list and add the string to it
-    public int index = 0;
+        userChoices = new string[14];
+
+        index = 0;
+    }
+
+    void HideAllMarks()
+    {
+        foreach (Image mark in answerMarks)
+        {
+            if (mark != null)
+            {
+                mark.gameObject.SetActive(false);
+            }
+        }
+    }
+
     public void addToUserChoices(string choice)
     {
-        if (index < userChoices.Length)
+        if (index >= userChoices.Length)
+            return;
+
+        userChoices[index] = choice;
+
+        // 顯示勾勾或叉叉
+        if (answerMarks[index] != null)
         {
-            userChoices[index] = choice;
-            index++;
-        }
-        // check if userchoices list is full of non-empty strings
-        if (index == userChoices.Length)
-        {
-            // if it is, compare it to correctAnswer list and if all is correct and order matches, enable button gameobject
-            bool allCorrect = false;
-            if (userChoices.SequenceEqual(correctAnswer))
+            if (choice == correctAnswer[index])
             {
-                allCorrect = true;
-                nextWinScreen.SetActive(true);
-                userChoices = new string[] { "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
-                currentScreen.SetActive(false);
-                Debug.Log("All answers are correct!");
-                //Debug.Log(userChoices);
-                //Debug.Log(correctAnswer);
-                Debug.Log(userChoices.SequenceEqual(correctAnswer));
-                index = 0;
+                answerMarks[index].sprite = checkSprite;
             }
             else
             {
-                // enable fail gameobject
-                failScreen.SetActive(true);
-                userChoices = new string[] { "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
-                currentScreen.SetActive(false);
-                Debug.Log("Some answers are incorrect!");
-                //Debug.Log(userChoices);
-                //Debug.Log(correctAnswer);
-                Debug.Log(userChoices.SequenceEqual(correctAnswer));
-                index = 0;
+                answerMarks[index].sprite = crossSprite;
             }
+
+            answerMarks[index].gameObject.SetActive(true);
+        }
+
+        index++;
+
+        // 全部答完
+        if (index == userChoices.Length)
+        {
+            if (userChoices.SequenceEqual(correctAnswer))
+            {
+                Debug.Log("All answers are correct!");
+
+                if (nextWinScreen != null)
+                    nextWinScreen.SetActive(true);
+
+                if (currentScreen != null)
+                    currentScreen.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Some answers are incorrect!");
+
+                if (failScreen != null)
+                    failScreen.SetActive(true);
+
+                if (currentScreen != null)
+                    currentScreen.SetActive(false);
+            }
+
+            index = 0;
         }
     }
-
-
 }
